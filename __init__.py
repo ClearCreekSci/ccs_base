71a12fc15417b77889e4ee71daa05bac2fd29cd5
Winslow <winslow@clearcreeksci.com>
2026-02-08 16:09:15 -0500

    addf functions, header etc.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,23 @@
+'''
+    ccs_base.__init__py
+    General purpose constansts, structures and functions for CCS development
+
+    Copyright (C) 2026 Clear Creek Scientific
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
 # DBus BLE advertisement
 CCS_DBUS_BLE_ADVERT_UUID     = 'a0ce0100-3bbf-11ee-89eb-00e04c400cc5'
 # DBus BLE agent
@@ -45,3 +65,106 @@ labels = {
     CCS_TURBIDITY_UUID :'Turbidity',
     CCS_PHOTOGRAPH_UUID:'Photograph',
 }
+
+UNKNOWN = 'Unknown'
+
+C_TO_F_CONVERSION = 1.8   # Add 32 after multiplying
+IN_HG_CONVERSION = 0.0295
+MS_TO_MPH_CONVERSION = 2.236936
+M_TO_IN_CONVERSION = 39.37008
+MM_TO_IN_CONVERSION = 0.03937008
+
+def getName(ID):
+    rv = UNKNOWN 
+    if ID in labels:
+        rv = labels[ID]
+    return rv
+
+def getValue(ID,v,metric):
+    rv = '' 
+    n = float(v)
+    if ID == CCS_AIR_TEMPERATURE_UUID:
+        if False == metric:
+            n *= C_TO_F_CONVERSION
+            n += 32
+        rv = '{:.{}f}'.format(n,2)
+    elif ID == CCS_HUMIDITY_UUID:
+        rv = '{:.{}f}'.format(n,2)
+    elif ID == CCS_AIR_PRESSURE_UUID:
+        if False == metric:
+            n *= IN_HG_CONVERSION 
+        rv = '{:.{}f}'.format(n,2)
+    elif ID == CCS_WIND_SPEED_UUID:
+        if False == metric:
+            n *= 2.235936
+        rv = '{:.{}f}'.format(n,2)
+    elif ID == CCS_WIND_DIRECTION_UUID:
+        rv = '{:.{}f}'.format(n,2)
+    elif ID == CCS_PRECIPITATION_UUID:
+        if False == metric:
+            n *= MM_TO_IN_CONVERSION 
+        rv = '{:.{}f}'.format(n,2)
+    elif ID == CCS_WATER_TEMPERATURE_UUID:
+        if False == metric:
+            n *= C_TO_F_CONVERSION
+            n += 32
+        rv = '{:.{}f}'.format(n,2)
+    elif ID == CCS_WATER_VELOCITY_UUID:
+        if False == metric:
+            n *= MS_TO_MPH_CONVERSION 
+        rv = '{:.{}f}'.format(n,2)
+    elif ID == CCS_WATER_LEVEL_UUID:
+        if False == metric:
+            n *= M_TO_IN_CONVERSION 
+        rv = '{:.{}f}'.format(n,2)
+    elif CCS_TURBIDITY_UUID:
+        rv = '{:.{}f}'.format(n,2)
+    else:
+        rv = '{:.{}f}'.format(n,2)
+    return rv
+
+def getUnits(ID,metric):
+    rv = '' 
+    if ID == CCS_AIR_TEMPERATURE_UUID:
+        if metric:
+            rv = '°C'
+        else:
+            rv = '°F'
+    elif ID == CCS_HUMIDITY_UUID:
+        rv = '%'
+    elif ID == CCS_AIR_PRESSURE_UUID:
+        if metric:
+            rv = 'hPa'
+        else:
+            rv = 'inHg'
+    elif ID == CCS_WIND_SPEED_UUID:
+        if metric:
+            rv = 'm/s'
+        else:
+            rv = 'mph'
+    elif ID == CCS_WIND_DIRECTION_UUID:
+        rv = '°'
+    elif ID == CCS_PRECIPITATION_UUID:
+        if metric:
+            rv = 'mm'
+        else:
+            rv = 'in'
+    elif ID == CCS_WATER_TEMPERATURE_UUID:
+        if metric:
+            rv = '°C'
+        else:
+            rv = '°F'
+    elif ID == CCS_WATER_VELOCITY_UUID:
+        if metric:
+            rv = 'm/s'
+        else:
+            rv = 'mph'
+    elif ID == CCS_WATER_LEVEL_UUID:
+        if metric:
+            rv = 'mm'
+        else:
+            rv = 'in'
+    elif CCS_TURBIDITY_UUID:
+            rv = 'NTU'
+    return rv
+
